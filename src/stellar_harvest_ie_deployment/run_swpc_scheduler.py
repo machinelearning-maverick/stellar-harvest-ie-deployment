fb27e3998e23a29ef05a2403a0ee18c5373bb1b5
Machine Learning Maverick <github@machinelearningmaverick.com>
2025-05-05 22:16:14 +0100

    feat(scheduler): Update Dockerfile and docker-compose for SWPC scheduler; add logging and job scheduling

--- src/stellar_harvest_ie_deployment/run_swpc_scheduler.py
+++ src/stellar_harvest_ie_deployment/run_swpc_scheduler.py
@@ -1,4 +1,35 @@
 import os
 import time
+import logging
 import schedule
+from stellar_harvest_ie_config.utils.log_decorators import log_io
 from stellar_harvest_ie_producers.stellar.swpc.producer import publish_swpc_record
+
+logger = logging.getLogger(__name__)
+
+
+@log_io()
+def job():
+    try:
+        publish_swpc_record()
+    except Exception as e:
+        logger.error("Failed: %s", e)
+
+
+def main():
+    kafka_uri = os.getenv("KAFKA_URI")
+    kafka_topic = os.getenv("KAFKA_TOPIC_SWPC")
+    logger.info(
+        "SWPC scheduler starting; broker: %s, topic: %s", kafka_uri, kafka_topic
+    )
+    schedule.every(30).minutes.do(job)
+
+    job()
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
